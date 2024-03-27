c161ac9df6db475a4c025870ccb30debd97c2b56
Sibin M S <sibin.ms@impress.ai>
2024-03-27 11:12:14 +0530

    typer based cli file called run.py

---
+++ run.py
@@ -0,0 +1,46 @@
+import typer
+from adapters.google_adapter import authenticate, get_service, fetch_emails, import_emails
+from handlers.rule_handler import rule_handler
+from schema.migrate import run_migrations
+
+app = typer.Typer()
+
+
+def authenticate_with_google():
+    """
+    Authenticates with Google and returns the Gmail service.
+    """
+    credentials = authenticate()
+    service = get_service('gmail', 'v1', credentials=credentials)
+    return service
+
+
+@app.command()
+def import_emails_from_inbox():
+    """
+    Imports emails from the gmail inbox using Oauth2.
+    """
+    service = authenticate_with_google()
+    messages = fetch_emails(service)
+    import_emails(messages, service)
+
+
+@app.command()
+def execute_rule():
+    """
+    Executes a rules on the emails such as mark_as_read/unread/move_to_inbox.
+    """
+    service = authenticate_with_google()
+    rule_handler(service)
+
+
+def main():
+    print("******************************* Gmail CLI **************************************")
+    print("---------- For importing emails from inbox and performing operations ----------")
+    print("-------------------------------------------------------------------------------\n\n")
+    run_migrations()
+    app()
+
+
+if __name__ == "__main__":
+    main()
